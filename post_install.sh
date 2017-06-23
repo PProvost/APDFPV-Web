0464f87efde54b3469e82d6cce00d63e689bb291
PProvost <peter@provost.org>
2017-06-23 14:52:12 -0600

    Getting sick of this...

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo ** Running bower install **
 ./node_modules/bower/bin/bower install
-echo ** Running grunt **
-./node_modules/grunt-cli/bin/grunt
+./node_modules/grunt-cli/bin/grunt --version --verbose
