61b4d653a3dda0b6c65abe03ced43eec15568ed1
PProvost <peter@provost.org>
2017-06-26 17:20:59 -0600

    Added favicons

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -30,6 +30,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/spin/javascripts/jquery.spin.js',
+      'bower_components/inherits/inherits.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
